d01d7edb2aeb9d17bff84ad59157a930176ddf07
Andrew Westgarth <mail@hawaythelads.co.uk>
2020-10-07 14:44:28 -0700

    Add project files.

---
+++ Data/MyDatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreSqlDb.Models
+{
+    public class MyDatabaseContext : DbContext
+    {
+        public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
+            : base(options)
+        {
+            var conn = (Microsoft.Data.SqlClient.SqlConnection)Database.GetDbConnection();
+            conn.AccessToken = (new Microsoft.Azure.Services.AppAuthentication.AzureServiceTokenProvider()).GetAccessTokenAsync("https://database.windows.net/").Result;
+        }
+
+        public DbSet<DotNetCoreSqlDb.Models.Todo> Todo { get; set; }
+    }
+}
